3c7d232ec599310804ebb26c483e38d6a9191fe5
Olimpia <olimpiafus@gmail.com>
2021-10-15 18:18:24 +0200

    ejercicio 1
    creacion de imagen y run para crear un container en docker

--- fundamentos/ejercicio 0/clone.md
+++
@@ -1,6 +0,0 @@
-!#/bin/bash
-
-clone olimpiaf99/edem2021
-
-touch hola.txt
-
---
+++ repaso fundamentos/Ejemplo1/main.py
@@ -0,0 +1,9 @@
+from datetime import datetime, date, timedelta
+import time
+from time import sleep
+import calendar
+ahora= datetime.now()
+while True:
+    # Print current time and sleep 1 minute
+    print("Fecha y Hora: ", ahora)
+    time.sleep(60)
